ddb1b5e857c36c7acd940a0dcb80ebd183adb2e8
nako <nhahn@cpro-cam.com>
2017-09-19 14:21:34 +0900

    add latex function

--- cgi/common.py
+++ cgi/common.py
@@ -26,7 +26,29 @@ handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=102400, backup
 
 logger.addHandler(handler)
 
-
+# for latex escape
+import re
+def tex_escape(text):
+    """
+        :param text: a plain text message
+        :return: the message escaped to appear correctly in LaTeX
+    """
+    conv = {
+        '&': r'\&',
+        '%': r'\%',
+        '$': r'\$',
+        '#': r'\#',
+        '_': r'\_',
+        '{': r'\{',
+        '}': r'\}',
+        '~': r'\textasciitilde{}',
+        '^': r'\^{}',
+        '\\': r'\textbackslash{}',
+        '<': r'\textless ',
+        '>': r'\textgreater ',
+    }
+    regex = re.compile('|'.join(re.escape(unicode(key)) for key in sorted(conv.keys(), key = lambda item: - len(item))))
+    return regex.sub(lambda match: conv[match.group()], text)
         
 class Udb:
     def __init__(self, path):
